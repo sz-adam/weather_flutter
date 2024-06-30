02cd984f19717c97a9ffa028a75da3d6182ae26a
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-06-30 13:15:15 +0200

    first character is uppercase

--- lib/widget/weather_text_field.dart
+++ lib/widget/weather_text_field.dart
@@ -19,6 +19,7 @@ class WeatherTextField extends StatelessWidget {
             border: OutlineInputBorder(),
             labelText: 'City',
           ),
+          textCapitalization: TextCapitalization.words,
           onSubmitted: onSubmitted,
         ),
       ),

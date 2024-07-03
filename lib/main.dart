22578d189b7e4613732c0446f0f1941ab6d0115d
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-07-03 17:16:00 +0200

    add bg color

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_weather/screen/home_screen.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart' ;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main()  async {
- await dotenv.load(fileName: ".env"); 
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       
-        home: HomeScreen());
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.lightBlue[700],
+      ),
+      home: const HomeScreen(),
+    );
   }
 }

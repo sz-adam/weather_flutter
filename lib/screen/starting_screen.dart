1e74afc9c22412a7e8b38596906f5a526bf3e1ed
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-07-05 18:00:36 +0200

    add bg image and add sending button

--- lib/screen/starting_screen.dart
+++ lib/screen/starting_screen.dart
@@ -19,38 +19,69 @@ class StartingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: _cityController,
-                decoration: const InputDecoration(
-                  labelText: 'Enter city name',
-                  border: OutlineInputBorder(),
-                ),
-                textCapitalization: TextCapitalization.words,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a city name';
-                  }
-                  return null;
-                },
-                // Ha a form érvényes, navigál a HomeScreen-re
-                onFieldSubmitted: (value) {
-                  if (_formKey.currentState?.validate() ?? false) {
-                    navigateToHomeScreen(context, value);
-                  }
-                },
-              ),
-              const SizedBox(height: 20),
-             
-            ],
+      body: Stack(
+        children: [
+          // Háttérkép beállítása
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/home.jpg',
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
+          // Átlátszó fekete réteg a háttérkép fölé
+          Positioned.fill(
+            child: Container(
+              color: Colors.black.withOpacity(0.3),
+            ),
+          ),
+          // A többi widget a háttérkép fölé kerül
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    controller: _cityController,
+                    decoration: InputDecoration(
+                      labelText: 'Enter city name',
+                      floatingLabelBehavior: FloatingLabelBehavior
+                          .never, // Ne lebegjen, hanem legyen a border alatt
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),                       
+                      ),
+                      filled: true, // Kitölti a hátteret fehérrel
+                      fillColor:
+                          Colors.white, // Fehér háttérszín a beviteli mezőnél                     
+                    ),
+                    textCapitalization: TextCapitalization.words,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a city name';
+                      }
+                      return null;
+                    },
+                    onFieldSubmitted: (value) {
+                      if (_formKey.currentState?.validate() ?? false) {
+                        navigateToHomeScreen(context, value);
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState?.validate() ?? false) {
+                        navigateToHomeScreen(context, _cityController.text);
+                      }
+                    },
+                    child: const Text('Sending'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
